a8866aaef5f05fceb5734f42f12897481129d0ea
avearmin <117543612+avearmin@users.noreply.github.com>
2023-07-04 23:14:45 -0400

    Add missing rows and columns to game logic

--- js/Grid.js
+++ js/Grid.js
@@ -1,15 +1,23 @@
 class Grid {
 
   constructor() {
-    this.row = 0;
-    this.col = 0;
+    this.col1 = 0;
+    this.col2 = 0;
+    this.col3 = 0;
+    this.row1 = 0;
+    this.row2 = 0;
+    this.row3 = 0;
     this.rDia = 0;
     this.lDia = 0;
   }
   
   reset() {
-    this.row = 0;
-    this.col = 0;
+    this.col1 = 0;
+    this.col2 = 0;
+    this.col3 = 0;
+    this.row1 = 0;
+    this.row2 = 0;
+    this.row3 = 0;
     this.rDia = 0;
     this.lDia = 0;
   }
--- js/Player.js
+++ js/Player.js
@@ -2,8 +2,12 @@ class Player {
 
   constructor() {
     this.isX = false;
-    this.col = 0;
-    this.row = 0;
+    this.col1 = 0;
+    this.col2 = 0;
+    this.col3 = 0;
+    this.row1 = 0;
+    this.row2 = 0;
+    this.row3 = 0;
     this.rDia = 0;
     this.lDia = 0;
   }
@@ -17,8 +21,12 @@ class Player {
   }
 
   reset() {
-    this.col = 0;
-    this.row = 0;
+    this.col1 = 0;
+    this.col2 = 0;
+    this.col3 = 0;
+    this.row1 = 0;
+    this.row2 = 0;
+    this.row3 = 0;
     this.rDia = 0;
     this.lDia = 0;
   }
