06f903f1720bde12a8014699af3850a9117ac70a
avearmin <117543612+avearmin@users.noreply.github.com>
2023-07-07 17:17:00 -0400

    Change EventHandler class name to ElementIds
    This change was made because the class is no longer planned to
    handle events. This functionality will be incorporated into a another
    class in the future.

---
+++ js/ElementIds.js
@@ -0,0 +1,14 @@
+class ElementIds {
+
+  constructor() {
+    this.cell_1 =  document.getElementById("cell-1");
+    this.cell_2 =  document.getElementById("cell-2");
+    this.cell_3 =  document.getElementById("cell-3");
+    this.cell_4 =  document.getElementById("cell-4");
+    this.cell_5 =  document.getElementById("cell-5");
+    this.cell_6 =  document.getElementById("cell-6");
+    this.cell_7 =  document.getElementById("cell-7");
+    this.cell_9 =  document.getElementById("cell-9");
+  }
+
+}
