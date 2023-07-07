85644bd1773d7d14e5a68b2d6e6b85a387c4dd0a
avearmin <117543612+avearmin@users.noreply.github.com>
2023-07-07 17:12:00 -0400

    Add increment/decrement cell methods
    These methods will be used by event listeners to increment/decrement
    the correct Grid data members when a particular cell is chosen by a
    player.

--- js/Grid.js
+++ js/Grid.js
@@ -11,6 +11,108 @@ class Grid {
     this.lDia = 0;
   }
   
+  incrementCell1() {
+    this.col1++;
+    this.row1++;
+    this.lDia++;
+  }
+
+  decrementCell1() {
+    this.col1--;
+    this.row1--;
+    this.lDia--;
+  }
+
+  incrementCell2() {
+    this.col2++;
+    this.row1++;
+  }
+
+  decrementCell2() {
+    this.col2--;
+    this.row1--;
+  }
+
+  incrementCell3() {
+    this.col3++;
+    this.row1++;
+    this.rDia++;
+  }
+
+  decrementCell3() {
+    this.col3--;
+    this.row1--;
+    this.rDia--;
+  }
+
+  incrementCell4() {
+    this.col1++;
+    this.row2++;
+  }
+
+  decrementCell4() {
+    this.col1--;
+    this.row2--;
+  }
+
+  incrementCell5() {
+    this.col2++;
+    this.row2++;
+    this.lDia++;
+    this.rDia++;
+  }
+
+  decrementCell5() {
+    this.col2--;
+    this.row2--;
+    this.lDia--;
+    this.rDia--;
+  }
+
+  incrementCell6() {
+    this.col3++;
+    this.row2++;
+  }
+
+  decrementCell6() {
+    this.col3++;
+    this.row2++;
+  }
+
+  incrementCell7() {
+    this.col1++;
+    this.row3++;
+    this.rDia++;
+  }
+
+  decrementCell7() {
+    this.col1--;
+    this.row3--;
+    this.rDia--;
+  }
+
+  incrementCell8() {
+    this.col2++;
+    this.row3++;
+  }
+
+  decrementCell8() {
+    this.col2--;
+    this.row3--;
+  }
+
+  incrementCell9() {
+    this.col3++;
+    this.row3++;
+    this.lDia++;
+  }
+
+  decrementCell9() {
+    this.col3++;
+    this.row3++;
+    this.lDia++;
+  }
+
   reset() {
     this.col1 = 0;
     this.col2 = 0;
